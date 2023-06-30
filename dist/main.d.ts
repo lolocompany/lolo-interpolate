0d21e3a9af603a38ea1373c2082fd080f0f498d2
Oliver Linnarsson <oliver@lolo.company>
2023-06-30 10:27:15 +0200

    feat: Migrate to typescript + add tests

---
+++ dist/main.d.ts
@@ -0,0 +1,7 @@
+/***************************************************************
+ * node/context/interpolate.js
+ *
+ * Copyright (C) Lolocodo AB - All rights reserved
+ ***************************************************************/
+export declare const interpolate: (ctx: unknown, params: unknown) => unknown;
+export default interpolate;

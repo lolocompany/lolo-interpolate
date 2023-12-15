552d2eab61c6d9978f144fb18e1deca065166294
Oliver Linnarsson <oliver@lolo.company>
2023-12-15 10:40:48 +0100

    feat: Move artifact to aws instead of inside repo

--- dist/main.d.ts
+++
@@ -1,7 +0,0 @@
-/***************************************************************
- * node/context/interpolate.js
- *
- * Copyright (C) Lolocodo AB - All rights reserved
- ***************************************************************/
-export declare const interpolate: (ctx: unknown, params: unknown) => unknown;
-export default interpolate;
